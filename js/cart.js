39a76868a3ed9bdfa4979561013592c4def43241
Ramses A Aquilina <3redjumper3@gmail.com>
2026-03-08 14:58:44 +0100

    Added inital (to be edited) images and icons to project. Additionally made website's basic layout and branding and added initial logic inspired by code from lectures (to be edited). All code will be properly cited in a later commit.

---
+++ js/cart.js
@@ -0,0 +1,10 @@
+const cartGrid = document.querySelector(".cart-grid");
+
+let cartArr = JSON.parse(localStorage.carted);
+
+for (let i = 0; i < cartArr.length; i++) {
+  let cartElement = document.createElement("img");
+  cartElement.src = cartArr[i].img;
+  cartElement.style.width = "100%";
+  cartGrid.appendChild(cartElement);
+}
---
+++ js/script.js
@@ -0,0 +1,44 @@
+let cartIcons = document.getElementsByClassName("cart");
+
+let cartArr;
+
+if (localStorage.cart) {
+  cartArr = JSON.parse(localStorage.cart);
+} else {
+  cartArr = [];
+}
+
+for (let i = 0; i < cartIcons.length; i++) {
+  for (let j = 0; j < cartArr.length; j++) {
+    if (cartIcons[i].dataset.id === cartArr[j].id) {
+      cartIcons[i].src = "img/cart-in.png";
+    }
+  }
+}
+
+for (let i = 0; i < cartIcons.length; i++) {
+  cartIcons[i].addEventListener("click", saveToCart);
+}
+
+function saveToCart() {
+  let carted = {
+    id: this.dataset.id,
+    img: this.dataset.img,
+  };
+
+  for (let i = 0; i < cartArr.length; i++) {
+    if (this.dataset.id === cartArr[i].id) {
+      cartArr.splice(i, 1);
+    }
+  }
+
+  if (this.src.includes("outline")) {
+    this.src = "img/cart-in.png";
+
+    cartArr.push(carted);
+  } else {
+    this.src = "img/cart.png";
+  }
+
+  localStorage.carted = JSON.stringify(cartArr);
+}
